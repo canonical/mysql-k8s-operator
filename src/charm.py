a592d5379cd4a93daea597a6d758ce37423f5805
Jose C. Massón <jose.masson@canonical.coop>
2020-12-11 17:40:33 -0300

    WIP #15 - Add a method to check if mysql is running

--- src/charm.py
+++ src/charm.py
@@ -3,6 +3,8 @@
 # See LICENSE file for licensing details.
 
 import logging
+import mysql.connector
+from mysql.connector import errorcode
 
 from oci_image import OCIImageResource, OCIImageResourceError
 from ops.charm import CharmBase
@@ -15,6 +17,7 @@ from ops.model import (
 from ops.framework import StoredState
 
 logger = logging.getLogger(__name__)
+PEER = "mysql"
 
 
 class MySQLOperatorCharm(CharmBase):
@@ -28,13 +31,14 @@ class MySQLOperatorCharm(CharmBase):
         self.image = OCIImageResource(self, 'mysql-image')
         self.port = self.model.config['port']
         self.framework.observe(self.on.config_changed, self._on_config_changed)
+        self.framework.observe(self.on.config_changed, self.on_start)
         self._stored.set_default(things=[])
 
     def _on_config_changed(self, _):
         self._configure_pod()
 
     def _configure_pod(self):
-        """Configure the K8s pod spec for Graylog."""
+        """Configure the K8s pod spec for MySQL."""
         if not self.unit.is_leader():
             self.unit.status = ActiveStatus()
             return
@@ -72,7 +76,7 @@ class MySQLOperatorCharm(CharmBase):
                 'kubernetes': {
                     'readinessProbe': {
                         'exec': {
-                            'command': ["mysql", "-h", "127.0.0.1", "-e", "SELECT 1"]
+                            'command': ["mysql", "-u", "root", "-h", "127.0.0.1", "-p", "Password", "-e", "SELECT 1"]
                         },
                         "timeoutSeconds": 5,
                         "periodSeconds": 5,
@@ -91,6 +95,65 @@ class MySQLOperatorCharm(CharmBase):
         }
 
         return pod_spec
+
+    # Handles start event
+    def on_start(self, event):
+        """Initialize MySQL
+
+        This event handler is deferred if initialization of MySQL
+        replica set fails. By doing so it is gauranteed that another
+        attempt at initialization will be made.
+        """
+        if not self.unit.is_leader():
+            return
+
+        # FIXME: This is awful!!! Only for development purpouses.
+        # We need to find a better way to wait for MySQL pod is ready
+        import time
+        time.sleep(40)
+
+        if not self._mysql_is_ready():
+            message = "Waiting for MySQL Service"
+            self.unit.status = WaitingStatus(message)
+            logger.info(message)
+            event.defer()
+        else:
+            self.unit.status = ActiveStatus("MySQL Service is ready")
+            # TODO: If MySQL is up and running we have to create
+            # an admin user for InnoDB Cluster on all nodes
+
+    def _mysql_is_ready(self):
+        ready = False
+
+        # FIXME: We are harcoding the 0 unit for development porpouses
+        # We have to check if MySQL is ready in every unit.
+        hostname = self._get_unit_hostname('0')
+
+        try:
+            config = {
+                'user': 'root',
+                'password': 'Password',
+                'host': hostname,
+            }
+            cnx = mysql.connector.connect(**config)
+            logger.info("MySQL service is ready.")
+            ready = True
+        except mysql.connector.Error as err:
+            # TODO: Improve exceptions handling
+            logger.info("MySQL service is not ready yet.")
+        else:
+            cnx.close()
+
+        return ready
+
+
+    def _get_unit_hostname(self, _id: int) -> str:
+        """
+        Construct a DNS name for a MySQL unit
+        """
+        return "{}-{}.{}-endpoints".format(self.model.app.name,
+                                           _id,
+                                           self.model.app.name)
 
 
 if __name__ == "__main__":
