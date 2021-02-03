daab6a0da3d1ddc9bb81b5ff8184f7dc155af1bc
Jose C. Massón <jose.masson@canonical.coop>
2021-02-03 02:19:00 -0300

    WIP - reformatted file with black

--- tests/test_charm.py
+++ tests/test_charm.py
@@ -25,18 +25,26 @@ class TestCharm(unittest.TestCase):
             "MYSQL_DATABASE": "db_10",
         }
         self.harness.update_config(config_1)
-        self.assertEqual(self.harness.charm.env_config["MYSQL_ROOT_PASSWORD"], "D10S")
-        self.assertEqual(self.harness.charm.env_config["MYSQL_USER"], "DiegoArmando")
+        self.assertEqual(
+            self.harness.charm.env_config["MYSQL_ROOT_PASSWORD"], "D10S"
+        )
+        self.assertEqual(
+            self.harness.charm.env_config["MYSQL_USER"], "DiegoArmando"
+        )
         self.assertEqual(
             self.harness.charm.env_config["MYSQL_PASSWORD"], "SegurolaYHabana"
         )
-        self.assertEqual(self.harness.charm.env_config["MYSQL_DATABASE"], "db_10")
+        self.assertEqual(
+            self.harness.charm.env_config["MYSQL_DATABASE"], "db_10"
+        )
 
         config_2 = {
             "MYSQL_ROOT_PASSWORD": "",
         }
         self.harness.update_config(config_2)
-        self.assertEqual(len(self.harness.charm.env_config["MYSQL_ROOT_PASSWORD"]), 20)
+        self.assertEqual(
+            len(self.harness.charm.env_config["MYSQL_ROOT_PASSWORD"]), 20
+        )
 
     def test_pod_spec(self):
         config_1 = {
@@ -45,10 +53,15 @@ class TestCharm(unittest.TestCase):
         self.harness.update_config(config_1)
         pod_spec = self.harness.charm._build_pod_spec()
         self.assertEqual(pod_spec["containers"][0]["name"], "mysql")
-        self.assertEqual(pod_spec["containers"][0]["ports"][0]["containerPort"], 3306)
-        self.assertEqual(pod_spec["containers"][0]["ports"][0]["protocol"], "TCP")
         self.assertEqual(
-            pod_spec["containers"][0]["envConfig"]["MYSQL_ROOT_PASSWORD"], "D10S"
+            pod_spec["containers"][0]["ports"][0]["containerPort"], 3306
+        )
+        self.assertEqual(
+            pod_spec["containers"][0]["ports"][0]["protocol"], "TCP"
+        )
+        self.assertEqual(
+            pod_spec["containers"][0]["envConfig"]["MYSQL_ROOT_PASSWORD"],
+            "D10S",
         )
         self.assertEqual(pod_spec["version"], 3)
 
