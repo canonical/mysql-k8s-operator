12353a186787798afa44901cd03b171539f15195
Jose C. Massón <jose.masson@canonical.coop>
2021-04-07 17:46:31 -0300

    Improving check_coverage module docstring and exception

--- tests/check_coverage.py
+++ tests/check_coverage.py
@@ -1,22 +1,32 @@
-# This script checks the coverage and test if it keeps at least the same
-# Add this to your .gitlab-ci.yml
-#
-# coveragetest:
-#  ...
-#  cache:
-#    paths:
-#    - coverage_value.txt
-#  script:
-#   - bin/coverage run bin/test
-#   - bin/coverage report
-#   - bin/coverage report | python check_coverage.py
+#!/usr/bin/env python3
+# Copyright 2021 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+"""
+Very simple module to check unit tests coverage.
+
+If you are using github actions you can add something like this:
+
+    - name: Unit tests
+      run: |
+        coverage run -m unittest
+    - name: Test coverage
+      run: |
+        coverage report src/*.py | python tests/check_coverage.py
+
+
+"""
 
 import sys
 
 MIN_COVERAGE = 80
 
 
-def main():
+def check() -> None:
+    """
+    This function reads coverage module standard output and check if
+    TOTAL test coverage is better or worst than MIN_COVERAGE
+    """
     coverage_report = sys.stdin.read()
     if "TOTAL" not in coverage_report:
         print("No coverage data found in stdin. -> FAILING")
@@ -24,7 +34,13 @@ def main():
         sys.exit(1)
     # extract coverag (last element after whitspace without the `%` sign)
     # TOTAL                                        116     22    81%
-    coverage_value = int(coverage_report.split()[-1][:-1])
+    try:
+        coverage_value = int(coverage_report.split()[-1][:-1])
+    except ValueError:
+        print("Unable to convert TOTAL coverage to integer")
+        print(coverage_report)
+        sys.exit(1)
+
     exit_status = 0
     exit_status = int(coverage_value < MIN_COVERAGE)
     msg = [f"Current test coverage is {coverage_value}%"]
@@ -37,4 +53,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    check()
