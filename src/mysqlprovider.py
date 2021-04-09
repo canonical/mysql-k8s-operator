b42364018bd17d32b7591e5ef4eaf9882949d065
Jose C. Massón <jose.masson@canonical.coop>
2021-04-09 18:46:42 -0300

    WIP - Adding methods to Provider, tests, etc

--- src/mysqlprovider.py
+++ src/mysqlprovider.py
@@ -4,6 +4,8 @@
 
 import json
 import logging
+from mysqlserver import MySQL
+from ops.framework import StoredState
 from ops.relation import Provider
 
 logger = logging.getLogger(__name__)
@@ -14,18 +16,37 @@ class MySQLProvider(Provider):
     MySQLProvider class
     """
 
+    _stored = StoredState()
+
     def __init__(self, charm, name, provides):
         super().__init__(charm, name, provides)
         self.charm = charm
+        self._stored.set_default(consumers={})
         events = self.charm.on[name]
+
         self.framework.observe(
-            events.relation_changed, self.on_database_relation_changed
+            events.relation_joined, self._on_database_relation_joined
+        )
+        self.framework.observe(
+            events.relation_changed, self._on_database_relation_changed
+        )
+        self.framework.observe(
+            events.relation_broken, self._on_database_relation_broken
         )
 
     ##############################################
     #               RELATIONS                    #
     ##############################################
-    def on_database_relation_changed(self, event):
+    def _on_database_relation_joined(self, event):
+        rel_id = event.relation.id
+        creds = self.credentials(rel_id)
+        self.charm.mysql.new_user(creds)
+        hostname = self.charm.hostname
+        event.relation.data[self.charm.app]["username"] = creds["username"]
+        event.relation.data[self.charm.app]["password"] = creds["password"]
+        event.relation.data[self.charm.app]["hostname"] = hostname
+
+    def _on_database_relation_changed(self, event):
         """Ensure total number of databases requested are available"""
         if not self.charm.unit.is_leader():
             return
@@ -35,10 +56,10 @@ class MySQLProvider(Provider):
         dbs = data.get("databases")
         dbs_requested = json.loads(dbs) if dbs else []
         logger.debug("SERVER REQUEST DB %s", dbs_requested)
-        dbs_available = self.charm.mongo.databases
+        dbs_available = self.charm.mysql.databases
         logger.debug("SERVER AVAILABLE DB %s", dbs_available)
-
         missing = None
+
         if dbs_requested:
             if dbs_available:
                 missing = list(set(dbs_requested) - set(dbs_available))
@@ -47,7 +68,41 @@ class MySQLProvider(Provider):
 
         if missing:
             dbs_available.extend(missing)
-            logger.debug("SERVER REQUEST RESPONSE %s", dbs_available)
+            logger.debug("SERVER REQUEST RESPONSE {}".format(dbs_available))
+            rel_id = event.relation.id
+            creds = self.credentials(rel_id)
+            self.charm.mysql.new_dbs_and_user(creds, dbs_available)
             event.relation.data[self.charm.app]["databases"] = json.dumps(
                 dbs_available
             )
+
+    def _on_database_relation_broken(self, event):
+        rel_id = event.relation.id
+        databases = json.loads(
+            event.relation.data[self.charm.app]["databases"]
+        )
+
+        if rel_id in self._stored.consumers:
+            creds = self.credentials(rel_id)
+            self.charm.mysql.remove_user(creds["username"])
+            _ = self._stored.consumers.pop(rel_id)
+
+        if self.charm.model.config["autodelete"]:
+            self.charm.mysql.drop_databases(databases)
+
+    def is_new_relation(self, rel_id) -> bool:
+        if rel_id in self._stored.consumers:
+            return False
+        else:
+            return True
+
+    def credentials(self, rel_id) -> dict:
+        if self.is_new_relation(rel_id):
+            creds = {
+                "username": self.new_username(rel_id),
+                "password": MySQL.new_password(),
+            }
+            self._stored.consumers[rel_id] = creds
+        else:
+            creds = self._stored.consumers[rel_id]
+        return creds
