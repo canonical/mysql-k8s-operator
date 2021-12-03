132e4bc65d7aefe16fcec7162aab76a95c63ebf8
Daniela Plascencia <daniela.plascencia@canonical.com>
2021-12-03 17:38:32 -0600

    Uses operator-template to re-define repo structure

--- src/charm.py
+++ src/charm.py
@@ -1,284 +1,104 @@
 #!/usr/bin/env python3
 # Copyright 2021 Canonical Ltd.
 # See LICENSE file for licensing details.
+#
+# Learn more at: https://juju.is/docs/sdk
+
+"""Charm the service.
+
+Refer to the following post for a quick-start guide that will help you
+develop a new k8s charm using the Operator Framework:
+
+    https://discourse.charmhub.io/t/4208
+"""
 
 import logging
 
-from mysqlprovider import MySQLProvider
-from mysqlserver import MySQL
 from ops.charm import CharmBase
 from ops.framework import StoredState
 from ops.main import main
-from ops.model import (
-    ActiveStatus,
-    MaintenanceStatus,
-    ModelError,
-    WaitingStatus,
-)
+from ops.model import ActiveStatus
 
 logger = logging.getLogger(__name__)
-PEER = "mysql"
-MYSQL_PORT = 3306
 
 
-class MySQLCharm(CharmBase):
-    """Charm to run MySQL on Kubernetes."""
+class OperatorTemplateCharm(CharmBase):
+    """Charm the service."""
 
     _stored = StoredState()
-    mysql_provider: MySQLProvider = None
 
     def __init__(self, *args):
         super().__init__(*args)
-        self._stored.set_default(
-            mysql_initialized=False,
-            pebble_ready=False,
-        )
-        self.framework.observe(
-            self.on.mysql_pebble_ready, self._on_pebble_ready
-        )
+        self.framework.observe(self.on.httpbin_pebble_ready, self._on_httpbin_pebble_ready)
         self.framework.observe(self.on.config_changed, self._on_config_changed)
-        self.framework.observe(self.on.update_status, self._on_update_status)
-        self.framework.observe(
-            self.on.create_user_action, self._on_create_user_action
-        )
-        self.framework.observe(
-            self.on.delete_user_action, self._on_delete_user_action
-        )
-        self.framework.observe(
-            self.on.create_database_action, self._on_create_database_action
-        )
-        self.framework.observe(
-            self.on.set_user_password_action, self._on_set_user_password_action
-        )
-        self._provide_mysql()
-        self.container = self.unit.get_container(PEER)
+        self.framework.observe(self.on.fortune_action, self._on_fortune_action)
+        self._stored.set_default(things=[])
 
-    ##############################################
-    #           CHARM HOOKS HANDLERS             #
-    ##############################################
-    def _on_pebble_ready(self, _):
-        self._stored.pebble_ready = True
-        self._update_peers()
-        self._configure_pod()
+    def _on_httpbin_pebble_ready(self, event):
+        """Define and start a workload using the Pebble API.
 
-    def _on_config_changed(self, _):
-        """Set a new Juju pod specification"""
-        self._update_peers()
-        self._configure_pod()
+        TEMPLATE-TODO: change this example to suit your needs.
+        You'll need to specify the right entrypoint and environment
+        configuration for your specific workload. Tip: you can see the
+        standard entrypoint of an existing container using docker inspect
 
-    def _on_update_status(self, _):
-        """Set status for all units
-        Status may be
-        - MySQL is not ready,
-        - MySQL is not Initialized
-        - Unit is active
+        Learn more about Pebble layers at https://github.com/canonical/pebble
         """
-
-        if not self.mysql.is_ready():
-            status_message = "MySQL not ready yet"
-            self.unit.status = WaitingStatus(status_message)
-            return
-
-        if not self._is_mysql_initialized():
-            status_message = "MySQL not initialized"
-            self.unit.status = WaitingStatus(status_message)
-            return
-
-        self.unit.status = ActiveStatus()
-
-    def _on_create_user_action(self, event):
-        """Handle the create_user action."""
-        creds = {
-            "username": event.params["username"],
-            "password": event.params["password"],
-            "hostname": "%",
-        }
-
-        try:
-            self.mysql.new_super_user(creds)
-            event.set_results({"username": creds["username"]})
-            event.log(f"Username {creds['username']} created")
-        except Exception as e:
-            logger.error(e)
-            event.fail(message=str(e))
-
-    def _on_set_user_password_action(self, event):
-        """Handle the set_user_password action."""
-        creds = {
-            "username": event.params["username"],
-            "password": event.params["password"],
-            "hostname": "%",
-        }
-
-        try:
-            self.mysql.set_user_password(creds)
-            event.set_results({"username": creds["username"]})
-            event.log(f"Pasword for username: {creds['username']} changed")
-        except Exception as e:
-            logger.error(e)
-            event.fail(message=str(e))
-
-    def _on_delete_user_action(self, event):
-        """Handle the create_user action."""
-
-        try:
-            self.mysql.drop_user(event.params["username"])
-            event.set_results({"username": event.params["username"]})
-            event.log(f"Username {event.params['username']} deleted")
-        except Exception as e:
-            logger.error(e)
-            event.fail(message=str(e))
-
-    def _on_create_database_action(self, event):
-        """Handle the create_user action."""
-
-        try:
-            self.mysql.new_database(event.params["database"])
-            event.set_results({"database": event.params["database"]})
-            event.log(f"Database {event.params['database']} created")
-        except Exception as e:
-            logger.error(e)
-            event.fail(message=str(e))
-
-    ##############################################
-    #               PROPERTIES                   #
-    ##############################################
-    @property
-    def mysql(self) -> MySQL:
-        """Returns MySQL object"""
-        peers_data = self.model.get_relation(PEER).data[self.app]
-        mysql_config = {
-            "app_name": self.model.app.name,
-            "host": self.unit_ip,
-            "port": MYSQL_PORT,
-            "user_name": "root",
-            "mysql_root_password": peers_data["mysql_root_password"],
-        }
-        return MySQL(mysql_config)
-
-    @property
-    def unit_ip(self) -> str:
-        """Returns unit's IP"""
-        if bind_address := self.model.get_binding(PEER).network.bind_address:
-            return str(bind_address)
-        return ""
-
-    ##############################################
-    #             UTILITY METHODS                #
-    ##############################################
-    def _mysql_root_password(self) -> str:
-        """
-        Returns mysql_root_password from the config or generates one.
-        """
-        return self.config["mysql_root_password"] or MySQL.new_password(20)
-
-    def _update_peers(self):
-        if self.unit.is_leader():
-            peers_data = self.model.get_relation(PEER).data[self.app]
-
-            if not peers_data.get("mysql_root_password"):
-                peers_data["mysql_root_password"] = self._mysql_root_password()
-
-    def _configure_pod(self):
-        """Configure the Pebble layer for MySQL."""
-        if not self._stored.pebble_ready:
-            msg = "Waiting for Pod startup to complete"
-            logger.debug(msg)
-            self.unit.status = MaintenanceStatus(msg)
-            return False
-
-        layer = self._build_pebble_layer()
-
-        if not layer["services"][PEER]["environment"].get(
-            "MYSQL_ROOT_PASSWORD", False
-        ):
-            msg = "Awaiting leader node to set mysql_root_password"
-            logger.debug(msg)
-            self.unit.status = MaintenanceStatus(msg)
-            return False
-
-        services = self.container.get_plan().to_dict().get("services", {})
-
-        if (
-            not services
-            or services[PEER]["environment"]
-            != layer["services"][PEER]["environment"]
-        ):
-            self.container.add_layer(PEER, layer, combine=True)
-            self._restart_service()
-            self.unit.status = ActiveStatus()
-            return True
-
-    def _build_pebble_layer(self):
-        """Construct the pebble layer"""
-        logger.debug("Building pebble layer")
-
-        def env_config() -> dict:
-            """Return the env_config for pebble layer"""
-            config = self.model.config
-            peers_data = self.model.get_relation(PEER).data[self.app]
-            env_config = {}
-            env_config["MYSQL_ROOT_PASSWORD"] = peers_data[
-                "mysql_root_password"
-            ]
-
-            if (user := config.get("mysql_user")) and (
-                password := config.get("mysql_password")
-            ):
-                env_config["MYSQL_USER"] = user
-                env_config["MYSQL_PASSWORD"] = password
-
-            if database := config.get("mysql_database"):
-                env_config["MYSQL_DATABASE"] = database
-
-            return env_config
-
-        layer = {
-            "summary": "MySQL layer",
-            "description": "Pebble layer configuration for MySQL",
+        # Get a reference the container attribute on the PebbleReadyEvent
+        container = event.workload
+        # Define an initial Pebble layer configuration
+        pebble_layer = {
+            "summary": "httpbin layer",
+            "description": "pebble config layer for httpbin",
             "services": {
-                PEER: {
+                "httpbin": {
                     "override": "replace",
-                    "summary": "mysql service",
-                    "command": "docker-entrypoint.sh mysqld",
+                    "summary": "httpbin",
+                    "command": "gunicorn -b 0.0.0.0:80 httpbin:app -k gevent",
                     "startup": "enabled",
-                    "environment": env_config(),
+                    "environment": {"thing": self.model.config["thing"]},
                 }
             },
         }
-
-        return layer
-
-    def _provide_mysql(self) -> None:
-        if self._is_mysql_initialized():
-            self.mysql_provider = MySQLProvider(
-                self, "database", PEER, self.mysql.version
-            )
-            self.mysql_provider.ready()
-            logger.debug("MySQL Provider is available")
-
-    def _restart_service(self):
-        """Restarts MySQL Service"""
-        try:
-            service = self.container.get_service(PEER)
-        except ConnectionError:
-            logger.debug("Pebble API is not yet ready")
-            return False
-        except ModelError:
-            logger.debug("MySQL service is not yet ready")
-            return False
-
-        if service.is_running():
-            self.container.stop(PEER)
-
-        self.container.start(PEER)
-        logger.debug("Restarted MySQL service")
+        # Add intial Pebble config layer using the Pebble API
+        container.add_layer("httpbin", pebble_layer, combine=True)
+        # Autostart any services that were defined with startup: enabled
+        container.autostart()
+        # Learn more about statuses in the SDK docs:
+        # https://juju.is/docs/sdk/constructs#heading--statuses
         self.unit.status = ActiveStatus()
-        self._stored.mysql_initialized = True
 
-    def _is_mysql_initialized(self) -> bool:
-        return self._stored.mysql_initialized
+    def _on_config_changed(self, _):
+        """Just an example to show how to deal with changed configuration.
+
+        TEMPLATE-TODO: change this example to suit your needs.
+        If you don't need to handle config, you can remove this method,
+        the hook created in __init__.py for it, the corresponding test,
+        and the config.py file.
+
+        Learn more about config at https://juju.is/docs/sdk/config
+        """
+        current = self.config["thing"]
+        if current not in self._stored.things:
+            logger.debug("found a new thing: %r", current)
+            self._stored.things.append(current)
+
+    def _on_fortune_action(self, event):
+        """Just an example to show how to receive actions.
+
+        TEMPLATE-TODO: change this example to suit your needs.
+        If you don't need to handle actions, you can remove this method,
+        the hook created in __init__.py for it, the corresponding test,
+        and the actions.py file.
+
+        Learn more about actions at https://juju.is/docs/sdk/actions
+        """
+        fail = event.params["fail"]
+        if fail:
+            event.fail(fail)
+        else:
+            event.set_results({"fortune": "A bug in the code is worth two in the documentation."})
 
 
 if __name__ == "__main__":
-    main(MySQLCharm)
+    main(OperatorTemplateCharm)
