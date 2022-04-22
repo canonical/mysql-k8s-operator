0e84336437eb4f571b5883f4109f7fb39e5f4b5a
Paulo Machado <paulo.machado@canonical.com>
2022-04-22 17:49:28 -0300

    integration test for multi unit

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -18,14 +18,14 @@ APP_NAME = METADATA["name"]
 
 @pytest.mark.abort_on_fail
 async def test_build_and_deploy(ops_test: OpsTest):
-    """Build the charm-under-test and deploy it together with related charms.
+    """Build the charm-under-test and deploy it.
 
     Assert on the unit status before any relations/configurations take place.
     """
     # build and deploy charm from local source folder
     charm = await ops_test.build_charm(".")
     resources = {"mysql-image": METADATA["resources"]["mysql-image"]["upstream-source"]}
-    await ops_test.model.deploy(charm, resources=resources, application_name=APP_NAME)
+    await ops_test.model.deploy(charm, resources=resources, application_name=APP_NAME, num_units=3)
 
     # issuing dummy update_status just to trigger an event
     await ops_test.model.set_config({"update-status-hook-interval": "10s"})
@@ -36,7 +36,11 @@ async def test_build_and_deploy(ops_test: OpsTest):
         raise_on_blocked=True,
         timeout=1000,
     )
+    assert len(ops_test.model.applications[APP_NAME].units) == 3
     assert ops_test.model.applications[APP_NAME].units[0].workload_status == "active"
+    assert ops_test.model.applications[APP_NAME].units[1].workload_status == "active"
+    assert ops_test.model.applications[APP_NAME].units[2].workload_status == "active"
 
     # effectively disable the update status from firing
     await ops_test.model.set_config({"update-status-hook-interval": "60m"})
+
