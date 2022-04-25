e09f3efdb6073f5c9354c1092a8acb32056e9147
Paulo Machado <paulo.machado@canonical.com>
2022-04-25 15:50:39 -0300

    fix image name to work in gh ci

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -17,7 +17,7 @@ APP_NAME = METADATA["name"]
 
 
 @pytest.mark.abort_on_fail
-async def test_build_and_deploy(ops_test: OpsTest):
+async def test_build_and_deploy_one(ops_test: OpsTest):
     """Build the charm-under-test and deploy it.
 
     Assert on the unit status before any relations/configurations take place.
@@ -25,10 +25,10 @@ async def test_build_and_deploy(ops_test: OpsTest):
     # build and deploy charm from local source folder
     charm = await ops_test.build_charm(".")
     resources = {"mysql-image": METADATA["resources"]["mysql-image"]["upstream-source"]}
-    await ops_test.model.deploy(charm, resources=resources, application_name=APP_NAME, num_units=3)
+    await ops_test.model.deploy(charm, resources=resources, application_name=APP_NAME, num_units=1)
 
     # issuing dummy update_status just to trigger an event
-    await ops_test.model.set_config({"update-status-hook-interval": "10s"})
+    await ops_test.model.set_config({"update-status-hook-interval": "60s"})
 
     await ops_test.model.wait_for_idle(
         apps=[APP_NAME],
@@ -36,11 +36,8 @@ async def test_build_and_deploy(ops_test: OpsTest):
         raise_on_blocked=True,
         timeout=1000,
     )
-    assert len(ops_test.model.applications[APP_NAME].units) == 3
+    assert len(ops_test.model.applications[APP_NAME].units) == 1
     assert ops_test.model.applications[APP_NAME].units[0].workload_status == "active"
-    assert ops_test.model.applications[APP_NAME].units[1].workload_status == "active"
-    assert ops_test.model.applications[APP_NAME].units[2].workload_status == "active"
 
     # effectively disable the update status from firing
     await ops_test.model.set_config({"update-status-hook-interval": "60m"})
-
