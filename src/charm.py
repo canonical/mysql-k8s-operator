adfacdcda328e2bb2bf520960c560f915d3893e2
Samuel Walladge <samuel.walladge@canonical.com>
2022-10-07 11:18:58 +1030

    Tidy get-cluster-status action output on failure
    `get_cluster_status()` returns None on failure,
    so that must be accounted for when setting the get-cluster-status
    action results dictionary.
    
    Also modify the get-cluster-status action output structure,
    to be more consistent between success and error output.

--- src/charm.py
+++ src/charm.py
@@ -508,7 +508,21 @@ class MySQLOperatorCharm(CharmBase):
 
     def _get_cluster_status(self, event: ActionEvent) -> None:
         """Get the cluster status without topology."""
-        event.set_results(self._mysql.get_cluster_status())
+        status = self._mysql.get_cluster_status()
+        if status:
+            event.set_results(
+                {
+                    "success": True,
+                    "status": status,
+                }
+            )
+        else:
+            event.set_results(
+                {
+                    "success": False,
+                    "message": "Failed to read cluster status.  See logs for more information.",
+                }
+            )
 
     def _restart(self, _) -> None:
         """Restart server rolling ops callback function.
