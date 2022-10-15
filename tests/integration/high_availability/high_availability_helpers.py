7a96379a790c052f41c42690779faddb70370064
shayancanonical <99665202+shayancanonical@users.noreply.github.com>
2022-10-15 09:39:39 -0400

    Implement kill primary, check re-election shakedown test (#101)
    * Add a test application for HA that writes to the database continuously
    
    * Update endpoints/read_only_endpoints upon cluster topology changes
    
    * Implement kill primary, check reelection shakedown test
    
    * Fix typo in CI workflow
    
    * Address lint warnings + fix failing unit tests
    
    * Address PR feedback + implement a HA replication test set up helper
    
    * Address failing lint warnings
    
    * Implement the replication check consistency integration test
    
    * Add test for no replication across clusters
    
    * Address lint warnings + remove another mysql application cluster at end of test
    
    * Address PR feedback
    
    * Address lint warnings
    
    * Temporarily move fixture into test file
    
    * Remove empty fixtures file
    
    * Add logic to ensure writes succeed after primary killed and re-elected
    
    * Address lint warnings
    
    * Fix error in CI workflow file
    
    * Fix bug in charm that accesses a nonexistent property
    
    * Add missing lightkube dependency for tls tests

---
+++ tests/integration/high_availability/high_availability_helpers.py
@@ -0,0 +1,206 @@
+# Copyright 2022 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+import logging
+from pathlib import Path
+from typing import Tuple
+
+import yaml
+from helpers import (
+    execute_queries_on_unit,
+    get_server_config_credentials,
+    get_unit_address,
+    is_relation_joined,
+    scale_application,
+)
+from juju.unit import Unit
+from pytest_operator.plugin import OpsTest
+
+# Copied these values from high_availability.application_charm.src.charm
+DATABASE_NAME = "continuous_writes_database"
+TABLE_NAME = "data"
+
+CLUSTER_NAME = "test_cluster"
+METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
+MYSQL_DEFAULT_APP_NAME = METADATA["name"]
+APPLICATION_DEFAULT_APP_NAME = "application"
+TIMEOUT = 15 * 60
+
+mysql_charm, application_charm = None, None
+
+logger = logging.getLogger(__name__)
+
+
+async def get_max_written_value_in_database(ops_test: OpsTest, unit: Unit) -> int:
+    """Retrieve the max written value in the MySQL database.
+
+    Args:
+        ops_test: The ops test framework
+        unit: The MySQL unit on which to execute queries on
+    """
+    server_config_credentials = await get_server_config_credentials(unit)
+    unit_address = await get_unit_address(ops_test, unit.name)
+
+    select_max_written_value_sql = [f"SELECT MAX(number) FROM `{DATABASE_NAME}`.`{TABLE_NAME}`;"]
+
+    output = await execute_queries_on_unit(
+        unit_address,
+        server_config_credentials["username"],
+        server_config_credentials["password"],
+        select_max_written_value_sql,
+    )
+
+    return output[0]
+
+
+async def get_application_name(ops_test: OpsTest, application_name: str) -> str:
+    """Returns the name of the application witt the provided application name.
+
+    This enables us to retrieve the name of the deployed application in an existing model.
+
+    Note: if multiple applications with the application name exist,
+    the first one found will be returned.
+    """
+    status = await ops_test.model.get_status()
+
+    for application in ops_test.model.applications:
+        # note that format of the charm field is not exactly "mysql" but instead takes the form
+        # of `local:focal/mysql-6`
+        if application_name in status["applications"][application]["charm"]:
+            return application
+
+    return None
+
+
+async def deploy_and_scale_mysql(
+    ops_test: OpsTest,
+    check_for_existing_application: bool = True,
+    mysql_application_name: str = MYSQL_DEFAULT_APP_NAME,
+) -> str:
+    """Deploys and scales the mysql application charm.
+
+    Args:
+        ops_test: The ops test framework
+        check_for_existing_application: Whether to check for existing mysql applications
+            in the model
+        mysql_application_name: The name of the mysql application if it is to be deployed
+    """
+    application_name = await get_application_name(ops_test, "mysql")
+
+    if check_for_existing_application and application_name:
+        if len(ops_test.model.applications[application_name].units) != 3:
+            async with ops_test.fast_forward():
+                await scale_application(ops_test, application_name, 3)
+
+        return application_name
+
+    global mysql_charm
+    if not mysql_charm:
+        charm = await ops_test.build_charm(".")
+        # Cache the built charm to avoid rebuilding it between tests
+        mysql_charm = charm
+
+    config = {"cluster-name": CLUSTER_NAME}
+    resources = {"mysql-image": METADATA["resources"]["mysql-image"]["upstream-source"]}
+
+    async with ops_test.fast_forward():
+        await ops_test.model.deploy(
+            mysql_charm,
+            application_name=mysql_application_name,
+            config=config,
+            resources=resources,
+            num_units=3,
+        )
+
+        await ops_test.model.wait_for_idle(
+            apps=[mysql_application_name],
+            status="active",
+            raise_on_blocked=True,
+            timeout=TIMEOUT,
+        )
+
+        assert len(ops_test.model.applications[mysql_application_name].units) == 3
+
+    return mysql_application_name
+
+
+async def deploy_and_scale_application(ops_test: OpsTest) -> str:
+    """Deploys and scales the test application charm.
+
+    Args:
+        ops_test: The ops test framework
+    """
+    application_name = await get_application_name(ops_test, "application")
+
+    if application_name:
+        if len(ops_test.model.applications[application_name].units) != 1:
+            async with ops_test.fast_forward():
+                await scale_application(ops_test, application_name, 1)
+
+        return application_name
+
+    global application_charm
+    if not application_charm:
+        charm = await ops_test.build_charm(
+            "./tests/integration/high_availability/application_charm/"
+        )
+        # Cache the built charm to avoid rebuilding it between tests
+        application_charm = charm
+
+    async with ops_test.fast_forward():
+        await ops_test.model.deploy(
+            application_charm,
+            application_name=APPLICATION_DEFAULT_APP_NAME,
+            num_units=1,
+        )
+
+        await ops_test.model.wait_for_idle(
+            apps=[APPLICATION_DEFAULT_APP_NAME],
+            status="waiting",
+            raise_on_blocked=True,
+            timeout=TIMEOUT,
+        )
+
+        assert len(ops_test.model.applications[APPLICATION_DEFAULT_APP_NAME].units) == 1
+
+    return APPLICATION_DEFAULT_APP_NAME
+
+
+async def relate_mysql_and_application(
+    ops_test: OpsTest, mysql_application_name: str, application_name: str
+) -> None:
+    """Relates the mysql and application charms.
+
+    Args:
+        ops_test: The ops test framework
+        mysql_application_name: The mysql charm application name
+        application_name: The continuous writes test charm application name
+    """
+    if is_relation_joined(ops_test, "database", "database"):
+        return
+
+    await ops_test.model.relate(
+        f"{application_name}:database", f"{mysql_application_name}:database"
+    )
+    await ops_test.model.block_until(lambda: is_relation_joined(ops_test, "database", "database"))
+
+    await ops_test.model.wait_for_idle(
+        apps=[mysql_application_name, application_name],
+        status="active",
+        raise_on_blocked=True,
+        timeout=TIMEOUT,
+    )
+
+
+async def high_availability_test_setup(ops_test: OpsTest) -> Tuple[str, str]:
+    """Run the set up for high availability tests.
+
+    Args:
+        ops_test: The ops test framework
+    """
+    mysql_application_name = await deploy_and_scale_mysql(ops_test)
+    application_name = await deploy_and_scale_application(ops_test)
+
+    await relate_mysql_and_application(ops_test, mysql_application_name, application_name)
+
+    return mysql_application_name, application_name
---
+++ tests/integration/high_availability/test_replication.py
@@ -0,0 +1,318 @@
+#!/usr/bin/env python3
+# Copyright 2022 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+import logging
+
+import lightkube
+import pytest
+from helpers import (
+    execute_queries_on_unit,
+    generate_random_string,
+    get_cluster_status,
+    get_primary_unit,
+    get_server_config_credentials,
+    get_unit_address,
+    scale_application,
+)
+from lightkube.resources.core_v1 import Pod
+from pytest_operator.plugin import OpsTest
+from tenacity import RetryError, Retrying, stop_after_delay, wait_fixed
+
+from tests.integration.high_availability.high_availability_helpers import (
+    deploy_and_scale_mysql,
+    get_application_name,
+    get_max_written_value_in_database,
+    high_availability_test_setup,
+)
+
+logger = logging.getLogger(__name__)
+
+TIMEOUT = 15 * 60
+
+
+@pytest.fixture(scope="module")
+async def continuous_writes(ops_test: OpsTest):
+    """Starts continuous writes to the MySQL cluster for a test and clear the writes at the end."""
+    application_name = await get_application_name(ops_test, "application")
+
+    application_unit = ops_test.model.applications[application_name].units[0]
+
+    stop_writes_action = await application_unit.run_action("stop-continuous-writes")
+    await stop_writes_action.wait()
+
+    start_writes_action = await application_unit.run_action("start-continuous-writes")
+    await start_writes_action.wait()
+
+    yield
+
+    clear_writes_action = await application_unit.run_action("clear-continuous-writes")
+    await clear_writes_action.wait()
+
+
+@pytest.mark.order(1)
+@pytest.mark.replication_tests
+async def test_build_and_deploy(ops_test: OpsTest) -> None:
+    """Simple test to ensure that the mysql and application charms get deployed."""
+    await high_availability_test_setup(ops_test)
+
+
+@pytest.mark.order(2)
+@pytest.mark.abort_on_fail
+@pytest.mark.replication_tests
+async def test_kill_primary_check_reelection(ops_test: OpsTest, continuous_writes) -> None:
+    """Test to kill the primary under load and ensure re-election of primary."""
+    mysql_application_name, _ = await high_availability_test_setup(ops_test)
+
+    mysql_unit = ops_test.model.applications[mysql_application_name].units[0]
+    primary = await get_primary_unit(ops_test, mysql_unit, mysql_application_name)
+    primary_name = primary.name
+
+    # kill the primary pod
+    client = lightkube.Client()
+    client.delete(Pod, primary.name.replace("/", "-"), namespace=ops_test.model.info.name)
+
+    async with ops_test.fast_forward():
+        # wait for model to stabilize, k8s will re-create the killed pod
+        await ops_test.model.wait_for_idle(
+            apps=[mysql_application_name],
+            status="active",
+            raise_on_blocked=True,
+            timeout=TIMEOUT,
+        )
+
+        # ensure a new primary was elected
+        mysql_unit = ops_test.model.applications[mysql_application_name].units[0]
+        new_primary = await get_primary_unit(ops_test, mysql_unit, mysql_application_name)
+        new_primary_name = new_primary.name
+
+        assert primary_name != new_primary_name
+
+        # wait (and retry) until the killed pod is back online in the mysql cluster
+        try:
+            for attempt in Retrying(stop=stop_after_delay(5 * 60), wait=wait_fixed(10)):
+                with attempt:
+                    cluster_status = await get_cluster_status(ops_test, mysql_unit)
+                    online_members = [
+                        label
+                        for label, member in cluster_status["defaultreplicaset"][
+                            "topology"
+                        ].items()
+                        if member["status"] == "online"
+                    ]
+                    assert len(online_members) == 3
+                    break
+        except RetryError:
+            assert False, "Old primary has not come back online after being killed"
+
+    last_written_value = await get_max_written_value_in_database(ops_test, primary)
+
+    for attempt in Retrying(stop=stop_after_delay(2 * 60), wait=wait_fixed(3)):
+        with attempt:
+            # ensure that all units are up to date (including the previous primary)
+            for unit in ops_test.model.applications[mysql_application_name].units:
+                written_value = await get_max_written_value_in_database(ops_test, unit)
+                assert written_value > last_written_value, "Continuous writes not incrementing"
+
+                last_written_value = written_value
+
+    # insert some data into the new primary and ensure that the writes get replicated
+    server_config_credentials = await get_server_config_credentials(new_primary)
+    new_primary_address = await get_unit_address(ops_test, new_primary_name)
+
+    value = generate_random_string(255)
+    database_name, table_name = "test-kill-primary-check-reelection", "data"
+    insert_value_sql = [
+        f"CREATE DATABASE IF NOT EXISTS `{database_name}`",
+        f"CREATE TABLE IF NOT EXISTS `{database_name}`.`{table_name}` (id varchar(255), primary key (id))",
+        f"INSERT INTO `{database_name}`.`{table_name}` (id) VALUES ('{value}')",
+    ]
+
+    await execute_queries_on_unit(
+        new_primary_address,
+        server_config_credentials["username"],
+        server_config_credentials["password"],
+        insert_value_sql,
+        commit=True,
+    )
+
+    select_value_sql = [
+        f"SELECT id FROM `{database_name}`.`{table_name}` WHERE id = '{value}'",
+    ]
+
+    try:
+        for attempt in Retrying(stop=stop_after_delay(5 * 60), wait=wait_fixed(10)):
+            with attempt:
+                for unit in ops_test.model.applications[mysql_application_name].units:
+                    unit_address = await get_unit_address(ops_test, unit.name)
+
+                    output = await execute_queries_on_unit(
+                        unit_address,
+                        server_config_credentials["username"],
+                        server_config_credentials["password"],
+                        select_value_sql,
+                    )
+                    assert output[0] == value
+    except RetryError:
+        assert False, "Cannot query inserted data from all units"
+
+    clean_up_database_and_table_sql = [
+        f"DROP TABLE IF EXISTS `{database_name}`.`{table_name}`",
+        f"DROP DATABASE IF EXISTS `{database_name}`",
+    ]
+
+    await execute_queries_on_unit(
+        new_primary_address,
+        server_config_credentials["username"],
+        server_config_credentials["password"],
+        clean_up_database_and_table_sql,
+        commit=True,
+    )
+
+
+@pytest.mark.order(3)
+@pytest.mark.abort_on_fail
+@pytest.mark.replication_tests
+async def test_check_consistency(ops_test: OpsTest, continuous_writes) -> None:
+    """Test to write to primary, and read the same data back from replicas."""
+    mysql_application_name, _ = await high_availability_test_setup(ops_test)
+
+    # assert that there are 3 units in the mysql cluster
+    assert len(ops_test.model.applications[mysql_application_name].units) == 3
+
+    mysql_unit = ops_test.model.applications[mysql_application_name].units[0]
+    primary = await get_primary_unit(ops_test, mysql_unit, mysql_application_name)
+    primary_address = await get_unit_address(ops_test, primary.name)
+
+    # insert some data into the mysql cluster
+    database_name, table_name = "test-check-consistency", "data"
+    inserted_value = generate_random_string(255)
+    server_config_credentials = await get_server_config_credentials(primary)
+    insert_data_sql = [
+        f"CREATE DATABASE IF NOT EXISTS `{database_name}`",
+        f"CREATE TABLE IF NOT EXISTS `{database_name}`.`{table_name}` (id varchar(255), primary key(id))",
+        f"INSERT INTO `{database_name}`.`{table_name}` (id) VALUES ('{inserted_value}')",
+    ]
+
+    await execute_queries_on_unit(
+        primary_address,
+        server_config_credentials["username"],
+        server_config_credentials["password"],
+        insert_data_sql,
+        commit=True,
+    )
+
+    # ensure that the inserted data gets replicated onto every instance in the mysql cluster
+    select_inserted_data_sql = [
+        f"SELECT id FROM `{database_name}`.`{table_name}` WHERE id = '{inserted_value}'",
+    ]
+
+    for unit in ops_test.model.applications[mysql_application_name].units:
+        unit_address = await get_unit_address(ops_test, unit.name)
+
+        try:
+            for attempt in Retrying(stop=stop_after_delay(10), wait=wait_fixed(2)):
+                with attempt:
+                    output = await execute_queries_on_unit(
+                        unit_address,
+                        server_config_credentials["username"],
+                        server_config_credentials["password"],
+                        select_inserted_data_sql,
+                    )
+                    assert output[0] == inserted_value
+        except RetryError:
+            assert False, f"Unable to query inserted data from unit {unit.name}"
+
+
+@pytest.mark.order(4)
+@pytest.mark.abort_on_fail
+@pytest.mark.replication_tests
+async def test_no_replication_across_clusters(ops_test: OpsTest, continuous_writes) -> None:
+    """Test to ensure that writes to one cluster do not replicate to another cluster."""
+    mysql_application_name, _ = await high_availability_test_setup(ops_test)
+
+    # assert that there are 3 units in the mysql cluster
+    assert len(ops_test.model.applications[mysql_application_name].units) == 3
+
+    # deploy another mysql application cluster with the same 'cluster-name'
+    another_mysql_application_name = "another"
+    await deploy_and_scale_mysql(
+        ops_test,
+        check_for_existing_application=False,
+        mysql_application_name=another_mysql_application_name,
+    )
+
+    # insert some data into the first/original mysql cluster
+    mysql_unit = ops_test.model.applications[mysql_application_name].units[0]
+    mysql_primary = await get_primary_unit(ops_test, mysql_unit, mysql_application_name)
+    mysql_primary_address = await get_unit_address(ops_test, mysql_primary.name)
+
+    database_name, table_name = "test-no-replication-across-clusters", "data"
+    inserted_value = generate_random_string(255)
+    server_config_credentials = await get_server_config_credentials(mysql_primary)
+    insert_data_sql = [
+        f"CREATE DATABASE IF NOT EXISTS `{database_name}`",
+        f"CREATE TABLE IF NOT EXISTS `{database_name}`.`{table_name}` (id varchar(255), primary key(id))",
+        f"INSERT INTO `{database_name}`.`{table_name}` (id) VALUES ('{inserted_value}')",
+    ]
+
+    await execute_queries_on_unit(
+        mysql_primary_address,
+        server_config_credentials["username"],
+        server_config_credentials["password"],
+        insert_data_sql,
+        commit=True,
+    )
+
+    # ensure that the inserted data gets replicated onto every instance in the mysql cluster
+    select_inserted_data_sql = [
+        f"SELECT id FROM `{database_name}`.`{table_name}` WHERE id = '{inserted_value}'",
+    ]
+
+    for unit in ops_test.model.applications[mysql_application_name].units:
+        unit_address = await get_unit_address(ops_test, unit.name)
+
+        try:
+            for attempt in Retrying(stop=stop_after_delay(10), wait=wait_fixed(2)):
+                with attempt:
+                    output = await execute_queries_on_unit(
+                        unit_address,
+                        server_config_credentials["username"],
+                        server_config_credentials["password"],
+                        select_inserted_data_sql,
+                    )
+                    assert output[0] == inserted_value
+        except RetryError:
+            assert False, f"Unable to query inserted data from unit {unit.name}"
+
+    # ensure that the inserted data DOES NOT get replicated into the another mysql cluster
+    another_mysql_unit = ops_test.model.applications[another_mysql_application_name].units[0]
+    another_mysql_primary = await get_primary_unit(
+        ops_test, another_mysql_unit, another_mysql_application_name
+    )
+    another_server_config_credentials = await get_server_config_credentials(another_mysql_primary)
+
+    select_databases_sql = [
+        "SELECT schema_name FROM information_schema.schemata",
+    ]
+
+    for unit in ops_test.model.applications[another_mysql_application_name].units:
+        unit_address = await get_unit_address(ops_test, unit.name)
+
+        output = await execute_queries_on_unit(
+            unit_address,
+            another_server_config_credentials["username"],
+            another_server_config_credentials["password"],
+            select_databases_sql,
+        )
+
+        assert len(output) > 0
+        assert "information_schema" in output
+        assert database_name not in output
+
+    # remove another mysql application cluster
+    await scale_application(ops_test, another_mysql_application_name, 0, wait=False)
+    await ops_test.model.remove_application(
+        another_mysql_application_name,
+        block_until_done=True,
+    )
