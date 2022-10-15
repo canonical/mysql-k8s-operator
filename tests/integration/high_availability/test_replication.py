55559c2e02abdc6f811bca1c8050467ce4b7d845
Shayan Patel <shayan.patel@canonical.com>
2022-10-15 09:48:49 -0400

    Address lint warnings

--- tests/integration/high_availability/test_replication.py
+++ tests/integration/high_availability/test_replication.py
@@ -9,7 +9,6 @@ import pytest
 from helpers import (
     execute_queries_on_unit,
     generate_random_string,
-    get_cluster_status,
     get_primary_unit,
     get_server_config_credentials,
     get_unit_address,
