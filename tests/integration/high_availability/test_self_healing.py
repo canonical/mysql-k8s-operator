c68971a2b84e91a4b1c7cb7062e4cd39225db2b9
Carl Csaposs <carl.csaposs@canonical.com>
2022-12-07 12:16:26 +0000

    Add missing await statements in integration tests (#122)

--- tests/integration/high_availability/test_self_healing.py
+++ tests/integration/high_availability/test_self_healing.py
@@ -154,7 +154,7 @@ async def test_freeze_db_process(ops_test: OpsTest, continuous_writes) -> None:
     async with ops_test.fast_forward():
         for attempt in Retrying(stop=stop_after_delay(60), wait=wait_fixed(10)):
             with attempt:
-                assert ensure_all_units_continuous_writes_incrementing(
+                await ensure_all_units_continuous_writes_incrementing(
                     ops_test, remaining_online_units
                 )
 
@@ -254,7 +254,7 @@ async def test_graceful_crash_of_primary(ops_test: OpsTest, continuous_writes) -
     async with ops_test.fast_forward():
         for attempt in Retrying(stop=stop_after_delay(60), wait=wait_fixed(10)):
             with attempt:
-                assert ensure_all_units_continuous_writes_incrementing(ops_test)
+                await ensure_all_units_continuous_writes_incrementing(ops_test)
 
 
 @pytest.mark.order(2)
