e4129d8acfd757904209faf77830affdd6de893c
Shayan Patel <shayan.patel@canonical.com>
2023-03-22 05:08:48 -0400

    Replace missed mysqld service import

--- tests/integration/high_availability/conftest.py
+++ tests/integration/high_availability/conftest.py
@@ -7,7 +7,7 @@ import logging
 import pytest
 from pytest_operator.plugin import OpsTest
 
-from constants import CONTAINER_NAME, MYSQLD_SERVICE
+from constants import CONTAINER_NAME, MYSQLD_SAFE_SERVICE
 
 from .high_availability_helpers import (
     deploy_chaos_mesh,
@@ -58,7 +58,7 @@ async def restart_policy(ops_test: OpsTest) -> None:
             ops_test,
             unit.name,
             CONTAINER_NAME,
-            MYSQLD_SERVICE,
+            MYSQLD_SAFE_SERVICE,
             "tests/integration/high_availability/manifests/extend_pebble_restart_delay.yml",
         )
 
@@ -78,7 +78,7 @@ async def restart_policy(ops_test: OpsTest) -> None:
             ops_test,
             unit.name,
             CONTAINER_NAME,
-            MYSQLD_SERVICE,
+            MYSQLD_SAFE_SERVICE,
             "tests/integration/high_availability/manifests/reduce_pebble_restart_delay.yml",
         )
 
