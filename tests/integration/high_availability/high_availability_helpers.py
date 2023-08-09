2ca44512ad8b91af0af893c82e7ea3999d6cfe65
Paulo Machado <paulo.machado@canonical.com>
2023-08-09 09:00:06 -0300

    fix call

--- tests/integration/high_availability/high_availability_helpers.py
+++ tests/integration/high_availability/high_availability_helpers.py
@@ -523,7 +523,7 @@ def isolate_instance_from_cluster(ops_test: OpsTest, unit_name: str) -> None:
         env["KUBECONFIG"] = os.path.expanduser("~/.kube/config")
 
         try:
-            subprocess.check_output(" ".join(["kubectl", "apply", "-f", temp_file.name]), env=env)
+            subprocess.check_output(["kubectl", "apply", "-f", temp_file.name], env=env)
         except subprocess.CalledProcessError as e:
             logger.error(e.output)
             logger.error(e.stderr)
