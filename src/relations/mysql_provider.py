ba9849bcef843cd08b5c2b7994d4cadf4258ea35
Judit Novak <judit.novak@canonical.com>
2023-10-13 14:41:01 +0200

    [DPE-2205] data-platform-libs v19 external secrets (#314)
    * data_platform_libs/data_interfaces.py v18 (Relation Secrets)
    
    * NO direct Relation databag usagegit add src/relations/mysql_provider.py!
    
    * Checking the correct usage of relation databag vs. secrets
    
    * Versions kept up-to-date for the pipeline

--- src/relations/mysql_provider.py
+++ src/relations/mysql_provider.py
@@ -74,10 +74,10 @@ class MySQLProvider(Object):
         Returns:
             str: The password.
         """
-        if password := relation.data[self.charm.app].get("password"):
+        if password := self.database.fetch_my_relation_field(relation.id, "password"):
             return password
         password = generate_random_password(PASSWORD_LENGTH)
-        relation.data[self.charm.app]["password"] = password
+        self.database.update_relation_data(relation.id, {"password": password})
         return password
 
     @staticmethod
