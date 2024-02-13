b2a77452f90f8435bec62efb3d2ee2ddf1ea6d93
Carl Csaposs <carl.csaposs@canonical.com>
2024-02-13 13:47:36 +0000

    Fix typo in secrets marker (#380)

--- tests/integration/markers.py
+++ tests/integration/markers.py
@@ -6,8 +6,8 @@ import pytest
 from . import juju_
 
 only_with_juju_secrets = pytest.mark.skipif(
-    not juju_.has_secrets, reason="Requires juju version w/o secrets"
+    not juju_.has_secrets, reason="Requires juju version w/secrets"
 )
 only_without_juju_secrets = pytest.mark.skipif(
-    juju_.has_secrets, reason="Requires juju version w/secrets"
+    juju_.has_secrets, reason="Requires juju version w/o secrets"
 )
