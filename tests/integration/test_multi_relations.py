bf9025f9ee48951d146e78027878df89e68532b3
Paulo Machado <paulo.machado@canonical.com>
2024-08-22 16:39:01 -0300

    increased scale

--- tests/integration/test_multi_relations.py
+++ tests/integration/test_multi_relations.py
@@ -10,6 +10,7 @@ import yaml
 from pytest_operator.plugin import OpsTest
 
 DB_METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
+SCALE_OUT = 7
 
 
 @pytest.mark.group(1)
@@ -26,13 +27,13 @@ async def test_build_and_deploy(ops_test: OpsTest):
         db_charm,
         application_name="mysql",
         config=config,
-        num_units=1,
+        num_units=3,
         resources=resources,
         series="jammy",
         trust=True,
     )
 
-    for i in range(1, 7):
+    for i in range(1, SCALE_OUT + 1):
         await ops_test.model.deploy(
             "mysql-test-app",
             application_name=f"app{i}",
@@ -52,7 +53,7 @@ async def test_build_and_deploy(ops_test: OpsTest):
 @pytest.mark.abort_on_fail
 async def test_relate_all(ops_test: OpsTest):
     """Relate all the applications to the database."""
-    for i in range(1, 7):
+    for i in range(1, SCALE_OUT + 1):
         await ops_test.model.relate("mysql:database", f"router{i}:backend-database")
         await ops_test.model.relate(f"app{i}:database", f"router{i}:database")
 
@@ -67,8 +68,8 @@ async def test_relate_all(ops_test: OpsTest):
 @pytest.mark.abort_on_fail
 async def test_scale_out(ops_test: OpsTest):
     """Scale database and routers."""
-    await ops_test.model.applications["mysql"].scale(3)
-    for i in range(1, 7):
+    await ops_test.model.applications["mysql"].scale(5)
+    for i in range(1, SCALE_OUT + 1):
         await ops_test.model.applications[f"router{i}"].scale(3)
     await ops_test.model.block_until(
         lambda: all(unit.workload_status == "active" for unit in ops_test.model.units.values()),
@@ -82,7 +83,7 @@ async def test_scale_out(ops_test: OpsTest):
 async def test_scale_in(ops_test: OpsTest):
     """Scale database and routers."""
     await ops_test.model.applications["mysql"].scale(1)
-    for i in range(1, 7):
+    for i in range(1, SCALE_OUT + 1):
         await ops_test.model.applications[f"router{i}"].scale(1)
     await ops_test.model.block_until(
         lambda: all(unit.workload_status == "active" for unit in ops_test.model.units.values()),
