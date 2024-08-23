cea086204bf2381462a069bc0fdd64b37879d6f0
Paulo Machado <paulo.machado@canonical.com>
2024-08-23 17:05:18 -0300

    use different dbs

--- tests/integration/test_multi_relations.py
+++ tests/integration/test_multi_relations.py
@@ -33,12 +33,14 @@ async def test_build_and_deploy(ops_test: OpsTest):
         trust=True,
     )
 
-    for i in range(1, SCALE_OUT + 1):
+    for i in range(SCALE_OUT):
+        config = {"database_name": f"database{i}"}
         await ops_test.model.deploy(
             "mysql-test-app",
             application_name=f"app{i}",
             num_units=1,
             channel="latest/edge",
+            config=config,
         )
         await ops_test.model.deploy(
             "mysql-router-k8s",
@@ -53,7 +55,7 @@ async def test_build_and_deploy(ops_test: OpsTest):
 @pytest.mark.abort_on_fail
 async def test_relate_all(ops_test: OpsTest):
     """Relate all the applications to the database."""
-    for i in range(1, SCALE_OUT + 1):
+    for i in range(SCALE_OUT):
         await ops_test.model.relate("mysql:database", f"router{i}:backend-database")
         await ops_test.model.relate(f"app{i}:database", f"router{i}:database")
 
@@ -69,7 +71,7 @@ async def test_relate_all(ops_test: OpsTest):
 async def test_scale_out(ops_test: OpsTest):
     """Scale database and routers."""
     await ops_test.model.applications["mysql"].scale(5)
-    for i in range(1, SCALE_OUT + 1):
+    for i in range(SCALE_OUT):
         await ops_test.model.applications[f"router{i}"].scale(3)
     await ops_test.model.block_until(
         lambda: all(unit.workload_status == "active" for unit in ops_test.model.units.values()),
@@ -83,7 +85,7 @@ async def test_scale_out(ops_test: OpsTest):
 async def test_scale_in(ops_test: OpsTest):
     """Scale database and routers."""
     await ops_test.model.applications["mysql"].scale(1)
-    for i in range(1, SCALE_OUT + 1):
+    for i in range(SCALE_OUT):
         await ops_test.model.applications[f"router{i}"].scale(1)
     await ops_test.model.block_until(
         lambda: all(unit.workload_status == "active" for unit in ops_test.model.units.values()),
