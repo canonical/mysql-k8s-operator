e7420fd95658bdc0d2d84c3dc7de8542beedb34e
shayancanonical <99665202+shayancanonical@users.noreply.github.com>
2025-02-27 06:38:02 -0500

    [DPE-1531] Avoid using initialize-insecure and instead use init-file to reset root user's password (#596)
    * Avoid using initialize-insecure and instead use init-file to reset root user's password
    
    * Address PR feedback

--- src/mysql_k8s_helpers.py
+++ src/mysql_k8s_helpers.py
@@ -41,6 +41,7 @@ from constants import (
     MYSQL_SYSTEM_GROUP,
     MYSQL_SYSTEM_USER,
     MYSQLD_DEFAULTS_CONFIG_FILE,
+    MYSQLD_INIT_CONFIG_FILE,
     MYSQLD_LOCATION,
     MYSQLD_SERVICE,
     MYSQLD_SOCK_FILE,
@@ -55,6 +56,10 @@ logger = logging.getLogger(__name__)
 
 if TYPE_CHECKING:
     from charm import MySQLOperatorCharm
+
+
+class MySQLResetRootPasswordAndStartMySQLDError(Error):
+    """Exception raised when there's an error resetting root password and starting mysqld."""
 
 
 class MySQLInitialiseMySQLDError(Error):
@@ -207,7 +212,7 @@ class MySQL(MySQLBase):
         """
         bootstrap_command = [
             MYSQLD_LOCATION,
-            "--initialize-insecure",
+            "--initialize",
             "-u",
             MYSQL_SYSTEM_USER,
         ]
@@ -223,6 +228,48 @@ class MySQL(MySQLBase):
             logger.exception("Failed to initialise MySQL data directory")
             self.reset_data_dir()
             raise MySQLInitialiseMySQLDError
+
+    def reset_root_password_and_start_mysqld(self) -> None:
+        """Reset the root user password and start mysqld."""
+        logger.debug("Resetting root user password and starting mysqld")
+        alter_user_queries = [
+            f"ALTER USER 'root'@'localhost' IDENTIFIED BY '{self.root_password}';",
+            "FLUSH PRIVILEGES;",
+        ]
+
+        self.container.push(
+            "/alter-root-user.sql",
+            "\n".join(alter_user_queries),
+            encoding="utf-8",
+            permissions=0o600,
+            user=MYSQL_SYSTEM_USER,
+            group=MYSQL_SYSTEM_GROUP,
+        )
+
+        try:
+            self.container.push(
+                MYSQLD_INIT_CONFIG_FILE,
+                "[mysqld]\ninit_file = /alter-root-user.sql",
+                encoding="utf-8",
+                permissions=0o600,
+                user=MYSQL_SYSTEM_USER,
+                group=MYSQL_SYSTEM_GROUP,
+            )
+        except PathError:
+            self.container.remove_path("/alter-root-user.sql")
+
+            logger.exception("Failed to write the custom config file for init-file")
+            raise
+
+        try:
+            self.container.restart(MYSQLD_SERVICE)
+            self.wait_until_mysql_connection(check_port=False)
+        except (TypeError, MySQLServiceNotRunningError):
+            logger.exception("Failed to run init-file and wait for connection")
+            raise
+        finally:
+            self.container.remove_path("/alter-root-user.sql")
+            self.container.remove_path(MYSQLD_INIT_CONFIG_FILE)
 
     @retry(reraise=True, stop=stop_after_delay(120), wait=wait_fixed(2))
     def wait_until_mysql_connection(self, check_port: bool = True) -> None:
