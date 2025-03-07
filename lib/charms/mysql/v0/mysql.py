cbcab2a7e82f634121f76752411b2198141a98c4
Paulo Machado <paulo.machado@canonical.com>
2025-03-07 13:42:06 -0300

    libpatch bump

--- lib/charms/mysql/v0/mysql.py
+++ lib/charms/mysql/v0/mysql.py
@@ -134,7 +134,7 @@ LIBID = "8c1428f06b1b4ec8bf98b7d980a38a8c"
 # Increment this major API version when introducing breaking changes
 LIBAPI = 0
 
-LIBPATCH = 84
+LIBPATCH = 85
 
 UNIT_TEARDOWN_LOCKNAME = "unit-teardown"
 UNIT_ADD_LOCKNAME = "unit-add"
