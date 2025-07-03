5f9e8ccf6959d394984ff8de83a0ce439e87d826
Paulo Machado <paulo.machado@canonical.com>
2025-07-03 17:40:56 -0300

    revert lib changes as its not needed

--- lib/charms/mysql/v0/mysql.py
+++ lib/charms/mysql/v0/mysql.py
@@ -133,7 +133,7 @@ LIBID = "8c1428f06b1b4ec8bf98b7d980a38a8c"
 # Increment this major API version when introducing breaking changes
 LIBAPI = 0
 
-LIBPATCH = 89
+LIBPATCH = 88
 
 UNIT_TEARDOWN_LOCKNAME = "unit-teardown"
 UNIT_ADD_LOCKNAME = "unit-add"
@@ -766,8 +766,6 @@ class MySQLCharmBase(CharmBase, ABC):
 
     def unit_initialized(self, raise_exceptions: bool = False) -> bool:
         """Check if the unit is added to the cluster."""
-        if not self.app_peer_data.get("cluster-name"):
-            return False
         try:
             return self._mysql.cluster_metadata_exists()
         except MySQLClusterMetadataExistsError:
