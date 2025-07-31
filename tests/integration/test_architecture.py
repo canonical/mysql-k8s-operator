671b59781185c9c0bee1793e2c3ca64819c11297
Carl Csaposs <carl.csaposs@canonical.com>
2025-07-31 15:31:30 +0200

    Add s390x integration tests

--- tests/integration/test_architecture.py
+++ tests/integration/test_architecture.py
@@ -55,4 +55,22 @@ async def test_amd_charm_on_arm_host(ops_test: OpsTest) -> None:
     )
 
 
-# TODO: add s390x test
+@markers.arm64_only
+async def test_amd_charm_on_s390x_host(ops_test: OpsTest) -> None:
+    """Tries deploying an amd64 charm on s390x host."""
+    charm = "./mysql-k8s_ubuntu@22.04-amd64.charm"
+
+    await ops_test.model.deploy(
+        charm,
+        application_name=APP_NAME,
+        num_units=1,
+        config={"profile": "testing"},
+        resources={"mysql-image": METADATA["resources"]["mysql-image"]["upstream-source"]},
+        base="ubuntu@22.04",
+    )
+
+    await ops_test.model.wait_for_idle(
+        apps=[APP_NAME],
+        status="error",
+        raise_on_error=False,
+    )
