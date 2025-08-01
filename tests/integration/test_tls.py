fceb214de5faa83536ae96cd087e6a56b6fe6e59
Carl Csaposs <carl.csaposs@canonical.com>
2025-08-01 12:27:27 +0200

    fix base

--- tests/integration/test_tls.py
+++ tests/integration/test_tls.py
@@ -123,9 +123,7 @@ async def test_enable_tls(ops_test: OpsTest) -> None:
     # Deploy TLS Certificates operator.
     logger.info("Deploy TLS operator")
     async with ops_test.fast_forward("60s"):
-        await ops_test.model.deploy(
-            tls_app_name, channel=tls_channel, config=tls_config, base="ubuntu@22.04"
-        )
+        await ops_test.model.deploy(tls_app_name, channel=tls_channel, config=tls_config)
         await ops_test.model.wait_for_idle(apps=[tls_app_name], status="active", timeout=15 * 60)
 
     # Relate with TLS charm
