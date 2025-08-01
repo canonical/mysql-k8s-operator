3045ec11b1c544701f08f771aefe4a1554c9ddc6
Sinclert Pérez <sinclert.perez@canonical.com>
2025-08-01 12:32:18 +0200

    Define relation-creation eagerly tests

--- tests/integration/relations/test_mysql_root.py
+++ tests/integration/relations/test_mysql_root.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Copyright 2022 Canonical Ltd.
+# Copyright 2021 Canonical Ltd.
 # See LICENSE file for licensing details.
 
 import asyncio
@@ -9,9 +9,6 @@ from pathlib import Path
 import pytest
 import yaml
 from pytest_operator.plugin import OpsTest
-
-from .. import markers
-from ..helpers import get_relation_data, is_relation_broken, is_relation_joined
 
 logger = logging.getLogger(__name__)
 
@@ -23,7 +20,7 @@ APPLICATION_APP_NAME = "mysql-test-app"
 
 APPS = [DATABASE_APP_NAME, APPLICATION_APP_NAME]
 
-ENDPOINT = "database"
+ENDPOINT = "mysql-root"
 
 
 @pytest.mark.abort_on_fail
@@ -84,60 +81,5 @@ async def test_relation_creation_eager(ops_test: OpsTest):
                 status="waiting",
                 raise_on_blocked=True,
                 timeout=1000,
-            ),
-        )
-
-
-@pytest.mark.abort_on_fail
-@markers.only_without_juju_secrets
-async def test_relation_creation_databag(ops_test: OpsTest):
-    """Relate charms and wait for the expected changes in status."""
-    await ops_test.model.relate(APPLICATION_APP_NAME, f"{DATABASE_APP_NAME}:{ENDPOINT}")
-
-    async with ops_test.fast_forward("60s"):
-        await ops_test.model.block_until(
-            lambda: is_relation_joined(ops_test, ENDPOINT, ENDPOINT) == True  # noqa: E712
-        )
-
-        await ops_test.model.wait_for_idle(apps=APPS, status="active")
-    relation_data = await get_relation_data(ops_test, APPLICATION_APP_NAME, "database")
-    assert {"password", "username"} <= set(relation_data[0]["application-data"])
-
-
-@pytest.mark.abort_on_fail
-@markers.only_with_juju_secrets
-async def test_relation_creation(ops_test: OpsTest):
-    """Relate charms and wait for the expected changes in status."""
-    await ops_test.model.relate(APPLICATION_APP_NAME, f"{DATABASE_APP_NAME}:{ENDPOINT}")
-
-    async with ops_test.fast_forward("60s"):
-        await ops_test.model.block_until(
-            lambda: is_relation_joined(ops_test, ENDPOINT, ENDPOINT) == True  # noqa: E712
-        )
-
-        await ops_test.model.wait_for_idle(apps=APPS, status="active")
-    relation_data = await get_relation_data(ops_test, APPLICATION_APP_NAME, "database")
-    assert not {"password", "username"} <= set(relation_data[0]["application-data"])
-    assert "secret-user" in relation_data[0]["application-data"]
-
-
-@pytest.mark.abort_on_fail
-async def test_relation_broken(ops_test: OpsTest):
-    """Remove relation and wait for the expected changes in status."""
-    await ops_test.model.applications[DATABASE_APP_NAME].remove_relation(
-        f"{APPLICATION_APP_NAME}:{ENDPOINT}", f"{DATABASE_APP_NAME}:{ENDPOINT}"
-    )
-
-    await ops_test.model.block_until(
-        lambda: is_relation_broken(ops_test, ENDPOINT, ENDPOINT) == True  # noqa: E712
-    )
-
-    async with ops_test.fast_forward("60s"):
-        await asyncio.gather(
-            ops_test.model.wait_for_idle(
-                apps=[DATABASE_APP_NAME], status="active", raise_on_blocked=True
-            ),
-            ops_test.model.wait_for_idle(
-                apps=[APPLICATION_APP_NAME], status="waiting", raise_on_blocked=True
             ),
         )
