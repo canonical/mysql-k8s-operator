30cd40cc4a1122b4ea265c5899aab883f514c30b
Sinclert Pérez <sinclert.perez@canonical.com>
2025-10-24 10:16:10 +0200

    Simplify arch inferring helper

--- tests/integration/architecture.py
+++ tests/integration/architecture.py
@@ -1,7 +1,9 @@
 # Copyright 2024 Canonical Ltd.
 # See LICENSE file for licensing details.
+
 import subprocess
 
-architecture = subprocess.run(
-    ["dpkg", "--print-architecture"], capture_output=True, check=True, encoding="utf-8"
-).stdout.strip()
+architecture = subprocess.check_output(
+    ["dpkg", "--print-architecture"],
+    text=True,
+).strip()
