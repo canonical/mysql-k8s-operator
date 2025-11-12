e4c2ab98f180f1fbcd7a386bec5f8650b960a68c
Juan Luis Cano Rodríguez <juan.rodriguez@canonical.com>
2025-11-12 16:43:16 +0100

    Migrate test_database.py to Jubilant

--- tests/integration/integration/relations/test_database.py
+++ tests/integration/integration/relations/test_database.py
@@ -2,19 +2,18 @@
 # Copyright 2022 Canonical Ltd.
 # See LICENSE file for licensing details.
 
-import asyncio
 import logging
 from pathlib import Path
 
+import jubilant_backports
 import pytest
 import yaml
-from pytest_operator.plugin import OpsTest
+from jubilant_backports import Juju
 
 from ... import markers
-from ...helpers import (
+from ...helpers_ha import (
     get_relation_data,
-    is_relation_broken,
-    is_relation_joined,
+    wait_for_apps_status,
 )
 
 logger = logging.getLogger(__name__)
@@ -31,112 +30,107 @@ APPS = [DATABASE_APP_NAME, APPLICATION_APP_NAME]
 
 @pytest.mark.abort_on_fail
 @pytest.mark.skip_if_deployed
-async def test_build_and_deploy(ops_test: OpsTest, charm):
+def test_build_and_deploy(juju: Juju, charm):
     """Build the charm and deploy 3 units to ensure a cluster is formed."""
     config = {"cluster-name": "test_cluster", "profile": "testing"}
     resources = {"mysql-image": DB_METADATA["resources"]["mysql-image"]["upstream-source"]}
 
-    await asyncio.gather(
-        ops_test.model.deploy(
-            charm,
-            application_name=DATABASE_APP_NAME,
-            config=config,
-            num_units=3,
-            resources=resources,
-            base="ubuntu@22.04",
-            trust=True,
-        ),
-        ops_test.model.deploy(
-            APPLICATION_APP_NAME,
-            application_name=APPLICATION_APP_NAME,
-            num_units=2,
-            channel="latest/edge",
-            base="ubuntu@22.04",
-        ),
+    juju.deploy(
+        charm,
+        DATABASE_APP_NAME,
+        config=config,
+        num_units=3,
+        resources=resources,
+        base="ubuntu@22.04",
+        trust=True,
+    )
+
+    juju.deploy(
+        APPLICATION_APP_NAME,
+        num_units=2,
+        channel="latest/edge",
+        base="ubuntu@22.04",
     )
 
 
 @pytest.mark.abort_on_fail
-async def test_relation_creation_eager(ops_test: OpsTest):
+def test_relation_creation_eager(juju: Juju):
     """Relate charms before they have time to properly start.
 
     It simulates a Terraform-like deployment strategy
     """
-    await ops_test.model.relate(
+    logger.info("Creating relation...")
+    juju.integrate(
         f"{APPLICATION_APP_NAME}:{APPLICATION_ENDPOINT}",
         f"{DATABASE_APP_NAME}:{DATABASE_ENDPOINT}",
     )
-    await ops_test.model.block_until(
-        lambda: is_relation_joined(ops_test, APPLICATION_ENDPOINT, DATABASE_ENDPOINT) == True  # noqa: E712
+
+    def count_units(status: jubilant_backports.Status, app_name: str, num_units: int) -> bool:
+        app = status.apps[app_name]
+        return len(app.units) == num_units
+
+    logger.info("Waiting for unit counts...")
+    juju.wait(
+        lambda status: count_units(status, DATABASE_APP_NAME, 3),
+    )
+    juju.wait(
+        lambda status: count_units(status, APPLICATION_APP_NAME, 2),
     )
 
-    # Reduce the update_status frequency until the cluster is deployed
-    async with ops_test.fast_forward("60s"):
-        await ops_test.model.block_until(
-            lambda: len(ops_test.model.applications[DATABASE_APP_NAME].units) == 3
-        )
-        await ops_test.model.block_until(
-            lambda: len(ops_test.model.applications[APPLICATION_APP_NAME].units) == 2
-        )
-
-        await asyncio.gather(
-            ops_test.model.wait_for_idle(
-                apps=[DATABASE_APP_NAME],
-                status="active",
-                raise_on_blocked=True,
-                timeout=1000,
-                raise_on_error=False,
-            ),
-            ops_test.model.wait_for_idle(
-                apps=[APPLICATION_APP_NAME],
-                status="waiting",
-                raise_on_blocked=True,
-                timeout=1000,
-            ),
-        )
+    logger.info("Waiting for application app to be waiting...")
+    juju.wait(
+        wait_for_apps_status(jubilant_backports.all_waiting, APPLICATION_APP_NAME),
+        error=jubilant_backports.any_blocked,
+        timeout=1000,
+    )
+    logger.info("Waiting for database app to be active...")
+    juju.wait(
+        wait_for_apps_status(jubilant_backports.all_active, DATABASE_APP_NAME),
+        error=jubilant_backports.any_blocked,
+        timeout=1000,
+    )
 
 
 @pytest.mark.abort_on_fail
 @markers.only_without_juju_secrets
-async def test_relation_creation_databag(ops_test: OpsTest):
+def test_relation_creation_databag(juju: Juju):
     """Relate charms and wait for the expected changes in status."""
-    async with ops_test.fast_forward("60s"):
-        await ops_test.model.wait_for_idle(apps=APPS, status="active")
+    juju.wait(jubilant_backports.all_active)
 
-    relation_data = await get_relation_data(ops_test, APPLICATION_APP_NAME, "database")
+    relation_data = get_relation_data(juju, APPLICATION_APP_NAME, "database")
     assert {"password", "username"} <= set(relation_data[0]["application-data"])
 
 
 @pytest.mark.abort_on_fail
 @markers.only_with_juju_secrets
-async def test_relation_creation(ops_test: OpsTest):
+def test_relation_creation(juju: Juju):
     """Relate charms and wait for the expected changes in status."""
-    async with ops_test.fast_forward("60s"):
-        await ops_test.model.wait_for_idle(apps=APPS, status="active")
+    juju.wait(jubilant_backports.all_active)
 
-    relation_data = await get_relation_data(ops_test, APPLICATION_APP_NAME, "database")
+    relation_data = get_relation_data(juju, APPLICATION_APP_NAME, "database")
     assert not {"password", "username"} <= set(relation_data[0]["application-data"])
     assert "secret-user" in relation_data[0]["application-data"]
 
 
 @pytest.mark.abort_on_fail
-async def test_relation_broken(ops_test: OpsTest):
+def test_relation_broken(juju: Juju):
     """Remove relation and wait for the expected changes in status."""
-    await ops_test.model.applications[DATABASE_APP_NAME].remove_relation(
+    juju.remove_relation(
         f"{APPLICATION_APP_NAME}:{APPLICATION_ENDPOINT}",
         f"{DATABASE_APP_NAME}:{DATABASE_ENDPOINT}",
     )
 
-    await ops_test.model.block_until(
-        lambda: is_relation_broken(ops_test, APPLICATION_ENDPOINT, DATABASE_ENDPOINT) == True  # noqa: E712
+    # Inspired by https://github.com/canonical/jubilant/blob/f9f785a/tests/integration/test_relations.py#L19-L24
+    juju.wait(
+        lambda status: not status.apps[APPLICATION_APP_NAME].relations
+        and not status.apps[DATABASE_APP_NAME].relations
     )
 
-    async with ops_test.fast_forward("60s"):
-        await asyncio.gather(
-            ops_test.model.wait_for_idle(
-                apps=[DATABASE_APP_NAME], status="active", raise_on_blocked=True
-            ),
-            ops_test.model.wait_for_idle(
-                apps=[APPLICATION_APP_NAME], status="waiting", raise_on_blocked=True
-            ),
-        )
+    juju.wait(
+        wait_for_apps_status(jubilant_backports.all_active, DATABASE_APP_NAME),
+        error=jubilant_backports.any_blocked,
+    )
+    juju.wait(
+        wait_for_apps_status(jubilant_backports.all_waiting, APPLICATION_APP_NAME),
+        error=jubilant_backports.any_blocked,
+    )
