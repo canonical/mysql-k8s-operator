4dc0afe875489fb42ea7a842371cc53a5067b1bf
Juan Luis Cano Rodríguez <juan.rodriguez@canonical.com>
2025-11-20 19:50:14 +0100

    Migrate backup_aws test

--- tests/integration/helpers_ha.py
+++ tests/integration/helpers_ha.py
@@ -440,6 +440,32 @@ def get_mysql_server_credentials(
     return credentials_task.results
 
 
+def rotate_mysql_server_credentials(
+    juju: Juju,
+    unit_name: str,
+    username: str = SERVER_CONFIG_USERNAME,
+    password: str | None = None,
+) -> None:
+    """Helper to run an action to rotate server config credentials.
+
+    Args:
+        juju: The Juju model
+        unit_name: The juju unit on which to run the rotate-password action for server-config credentials
+        username: The username to rotate the password for
+        password: The new password to set
+    """
+    params = {"username": username}
+    if password is not None:
+        params["password"] = password
+
+    rotate_task = juju.run(
+        unit=unit_name,
+        action="set-password",
+        params=params,
+    )
+    rotate_task.raise_on_failure()
+
+
 def get_mysql_max_written_value(juju: Juju, app_name: str, unit_name: str) -> int:
     """Retrieve the max written value in the MySQL database.
 
