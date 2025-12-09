4802498de20f1c2027e7815fdf1a14b28f66ab76
Juan Luis Cano Rodríguez <juan.rodriguez@canonical.com>
2025-12-09 15:47:29 +0100

    Migrate PITR GCP tests to Jubilant

---
+++ tests/integration/integration/test_backup_pitr_gcp_new.py
@@ -0,0 +1,21 @@
+# Copyright 2025 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+import pytest
+from jubilant_backports import Juju
+
+from .backups_ha import build_and_deploy_operations, pitr_operations
+
+
+@pytest.mark.abort_on_fail
+def test_build_and_deploy_gcp(
+    juju: Juju, cloud_configs_gcp: tuple[dict[str, str], dict[str, str]], charm
+) -> None:
+    """Build and deploy for GCP."""
+    build_and_deploy_operations(juju, charm, cloud_configs_gcp[0], cloud_configs_gcp[1])
+
+
+@pytest.mark.abort_on_fail
+def test_pitr_gcp(juju: Juju, cloud_configs_gcp: tuple[dict[str, str], dict[str, str]]) -> None:
+    """Pitr tests."""
+    pitr_operations(juju, cloud_configs_gcp[0], cloud_configs_gcp[1])
