96d89accad910af076e16baa10b3fb94a091f725
Juan Luis Cano Rodríguez <juan.rodriguez@canonical.com>
2025-12-11 12:10:14 +0100

    Fix Ceph backup test

--- tests/integration/integration/test_backup_ceph_new.py
+++ tests/integration/integration/test_backup_ceph_new.py
@@ -19,12 +19,10 @@ from jubilant_backports import Juju
 
 from constants import CLUSTER_ADMIN_USERNAME, ROOT_USERNAME, SERVER_CONFIG_USERNAME
 
-from ..helpers import generate_random_string
+from ..helpers import execute_queries_on_unit, generate_random_string
 from ..helpers_ha import (
     CHARM_METADATA,
     MINUTE_SECS,
-    TEST_DATABASE_NAME,
-    execute_queries_on_unit,
     get_app_units,
     get_mysql_primary_unit,
     get_mysql_server_credentials,
@@ -33,6 +31,7 @@ from ..helpers_ha import (
     rotate_mysql_server_credentials,
     scale_app_units,
     wait_for_apps_status,
+    wait_for_unit_status,
 )
 
 logger = logging.getLogger(__name__)
@@ -56,7 +55,7 @@ MOVE_RESTORED_CLUSTER_TO_ANOTHER_S3_REPOSITORY_ERROR = (
 
 logging.getLogger("jubilant.wait").setLevel(logging.WARNING)
 
-backup_id, backups_by_cloud, value_before_backup, value_after_backup = None, None, None, None
+backup_id, value_before_backup, value_after_backup = None, None, None
 MICROCEPH_BUCKET = "testbucket"
 
 
@@ -154,35 +153,35 @@ def clean_backups_from_buckets(cloud_credentials, cloud_configs):
         bucket_object.delete()
 
 
+@pytest.mark.abort_on_fail
 def test_build_and_deploy(juju: Juju, charm) -> None:
     """Simple test to ensure that the mysql charm gets deployed."""
     juju.deploy(
         charm,
         DATABASE_APP_NAME,
-        num_units=3,
-        resources={"mysql-image": CHARM_METADATA["resources"]["mysql-image"]["upstream-source"]},
         base="ubuntu@22.04",
         config={"cluster-name": CLUSTER_NAME, "profile": "testing"},
+        num_units=3,
+        resources={"mysql-image": CHARM_METADATA["resources"]["mysql-image"]["upstream-source"]},
         trust=True,
     )
 
     juju.wait(
         ready=wait_for_apps_status(jubilant_backports.all_active, DATABASE_APP_NAME),
-        timeout=TIMEOUT,
+        error=jubilant_backports.any_blocked,
+        timeout=15 * MINUTE_SECS,
     )
 
-    primary_unit = get_mysql_primary_unit(juju, DATABASE_APP_NAME)
+    primary_unit_name = get_mysql_primary_unit(juju, DATABASE_APP_NAME)
 
     logger.info("Rotating all mysql credentials")
     rotate_mysql_server_credentials(
-        juju, primary_unit, username=CLUSTER_ADMIN_USERNAME, password=CLUSTER_ADMIN_PASSWORD
+        juju, primary_unit_name, CLUSTER_ADMIN_USERNAME, CLUSTER_ADMIN_PASSWORD
     )
     rotate_mysql_server_credentials(
-        juju, primary_unit, username=SERVER_CONFIG_USERNAME, password=SERVER_CONFIG_PASSWORD
+        juju, primary_unit_name, SERVER_CONFIG_USERNAME, SERVER_CONFIG_PASSWORD
     )
-    rotate_mysql_server_credentials(
-        juju, primary_unit, username=ROOT_USERNAME, password=ROOT_PASSWORD
-    )
+    rotate_mysql_server_credentials(juju, primary_unit_name, ROOT_USERNAME, ROOT_PASSWORD)
 
     logger.info("Deploying s3-integrator")
 
@@ -196,20 +195,20 @@ def test_build_and_deploy(juju: Juju, charm) -> None:
 
 
 @pytest.mark.abort_on_fail
-def test_backup(juju: Juju, charm, cloud_credentials, cloud_configs) -> None:
+def test_backup(juju: Juju, cloud_credentials, cloud_configs) -> None:
     """Test to create a backup and list backups."""
-    global backup_id, backups_by_cloud, value_before_backup, value_after_backup
+    global backup_id, value_before_backup, value_after_backup
 
     app_units = get_app_units(juju, DATABASE_APP_NAME)
-    zeroth_unit = app_units[0]
+    zeroth_unit_name = app_units[0]
 
-    primary_unit = get_mysql_primary_unit(juju, DATABASE_APP_NAME, zeroth_unit)
-    non_primary_units = [unit for unit in app_units if unit != primary_unit]
+    primary_unit_name = get_mysql_primary_unit(juju, DATABASE_APP_NAME)
+    non_primary_unit_names = [unit for unit in app_units if unit != primary_unit_name]
 
-    # insert data into cluster before
+    # insert data into cluster before backup
     logger.info("Inserting value before backup")
     value_before_backup = generate_random_string(255)
-    credentials = get_mysql_server_credentials(juju, primary_unit)
+    credentials = get_mysql_server_credentials(juju, primary_unit_name)
     insert_mysql_data_and_validate_replication(
         juju, DATABASE_APP_NAME, DATABASE_NAME, TABLE_NAME, value_before_backup, credentials
     )
@@ -217,14 +216,12 @@ def test_backup(juju: Juju, charm, cloud_credentials, cloud_configs) -> None:
     logger.info("Setting s3 config")
     juju.config(S3_INTEGRATOR, cloud_configs)
     logger.info("Syncing credentials")
-    s3_unit = get_app_units(juju, S3_INTEGRATOR)[0]
-    task = juju.run(
-        unit=s3_unit,
+    s3_unit_name = get_app_units(juju, S3_INTEGRATOR)[0]
+    juju.run(
+        unit=s3_unit_name,
         action="sync-s3-credentials",
         params=cloud_credentials,
-        wait=5 * MINUTE_SECS,
     )
-    task.raise_on_failure()
 
     juju.wait(
         ready=wait_for_apps_status(
@@ -235,23 +232,22 @@ def test_backup(juju: Juju, charm, cloud_credentials, cloud_configs) -> None:
 
     # list backups
     logger.info("Listing existing backup ids")
-    results = juju.run(zeroth_unit, "list-backups")
-    results.raise_on_failure()
-    output = results.results["backups"]
+
+    results = juju.run(zeroth_unit_name, "list-backups").results
+    output = results["backups"]
     backup_ids = [line.split("|")[0].strip() for line in output.split("\n")[2:]]
 
     # create backup
     logger.info("Creating backup")
-    results = juju.run(non_primary_units[0], "create-backup", wait=5 * MINUTE_SECS)
-    results.raise_on_failure()
-    backup_id = results.results["backup-id"]
+
+    results = juju.run(non_primary_unit_names[0], "create-backup", wait=5 * MINUTE_SECS).results
+    backup_id = results["backup-id"]
 
     # list backups again and ensure new backup id exists
     logger.info("Listing backup ids post backup")
 
-    results = juju.run(zeroth_unit, "list-backups")
-    results.raise_on_failure()
-    output = results.results["backups"]
+    results = juju.run(zeroth_unit_name, "list-backups").results
+    output = results["backups"]
     new_backup_ids = [line.split("|")[0].strip() for line in output.split("\n")[2:]]
 
     assert sorted(new_backup_ids) == sorted([*backup_ids, backup_id])
@@ -265,26 +261,24 @@ def test_backup(juju: Juju, charm, cloud_credentials, cloud_configs) -> None:
 
 
 @pytest.mark.abort_on_fail
-def test_restore_on_same_cluster(juju: Juju, charm, cloud_credentials, cloud_configs) -> None:
+def test_restore_on_same_cluster(juju: Juju, cloud_credentials, cloud_configs) -> None:
     """Test to restore a backup to the same mysql cluster."""
     logger.info("Scaling mysql application to 1 unit")
     scale_app_units(juju, DATABASE_APP_NAME, 1)
 
-    mysql_unit = get_app_units(juju, DATABASE_APP_NAME)[0]
-    mysql_unit_address = get_unit_address(juju, DATABASE_APP_NAME, mysql_unit)
+    mysql_unit_name = get_app_units(juju, DATABASE_APP_NAME)[0]
+    mysql_unit_address = get_unit_address(juju, DATABASE_APP_NAME, mysql_unit_name)
 
     # set the s3 config and credentials
     logger.info("Syncing credentials")
 
     juju.config(S3_INTEGRATOR, cloud_configs)
-    s3_unit = get_app_units(juju, S3_INTEGRATOR)[0]
-    task = juju.run(
-        unit=s3_unit,
+    s3_unit_name = get_app_units(juju, S3_INTEGRATOR)[0]
+    juju.run(
+        unit=s3_unit_name,
         action="sync-s3-credentials",
         params=cloud_credentials,
-        wait=5 * MINUTE_SECS,
     )
-    task.raise_on_failure()
 
     juju.wait(
         ready=wait_for_apps_status(
@@ -295,17 +289,18 @@ def test_restore_on_same_cluster(juju: Juju, charm, cloud_credentials, cloud_con
 
     # restore the backup
     logger.info(f"Restoring {backup_id=}")
-    results = juju.run(
-        mysql_unit, "restore", params={"backup-id": backup_id}, wait=10 * MINUTE_SECS
-    )
-    results.raise_on_failure()
+
+    juju.run(mysql_unit_name, "restore", params={"backup-id": backup_id})
 
     # ensure the correct inserted values exist
     logger.info(
-        "Ensuring that the pre-backup inserted value exists in database, while post-backup inserted value does not"
+        "Ensuring that the pre-backup inserted value exists in database, "
+        "while post-backup inserted value does not"
     )
-    credentials = get_mysql_server_credentials(juju, mysql_unit)
-    select_values_sql = [f"SELECT id FROM `{TEST_DATABASE_NAME}`.`{TABLE_NAME}`"]
+    select_values_sql = [f"SELECT id FROM `{DATABASE_NAME}`.`{TABLE_NAME}`"]
+
+    primary_unit_name = get_app_units(juju, DATABASE_APP_NAME)[0]
+    credentials = get_mysql_server_credentials(juju, primary_unit_name)
 
     values = execute_queries_on_unit(
         mysql_unit_address,
@@ -323,6 +318,7 @@ def test_restore_on_same_cluster(juju: Juju, charm, cloud_credentials, cloud_con
     )
 
     logger.info("Ensuring that pre-backup and post-restore values exist in the database")
+
     values = execute_queries_on_unit(
         mysql_unit_address,
         credentials["username"],
@@ -333,22 +329,30 @@ def test_restore_on_same_cluster(juju: Juju, charm, cloud_credentials, cloud_con
     assert sorted(values) == sorted([value_before_backup, value_after_restore])
 
     logger.info("Scaling mysql application to 3 units")
-    scale_app_units(juju, DATABASE_APP_NAME, 3)
+    juju.add_unit(DATABASE_APP_NAME, num_units=2)
+
     juju.wait(
-        ready=wait_for_apps_status(jubilant_backports.all_active, DATABASE_APP_NAME),
+        ready=lambda status: all((
+            jubilant_backports.all_agents_idle(status, DATABASE_APP_NAME),
+            *(
+                wait_for_unit_status(DATABASE_APP_NAME, unit_name, "active")(status)
+                for unit_name in status.get_units(DATABASE_APP_NAME)
+            ),
+        )),
         timeout=TIMEOUT,
     )
 
     logger.info("Ensuring inserted values before backup and after restore exist on all units")
-    for unit in get_app_units(juju, DATABASE_APP_NAME):
-        mysql_unit_address = get_unit_address(juju, DATABASE_APP_NAME, unit)
+    for unit_name in get_app_units(juju, DATABASE_APP_NAME):
+        unit_address = get_unit_address(juju, DATABASE_APP_NAME, unit_name)
+
         values = execute_queries_on_unit(
-            mysql_unit_address,
+            unit_address,
             credentials["username"],
             credentials["password"],
             select_values_sql,
         )
-        assert value_before_backup
+
         assert sorted(values) == sorted([value_before_backup, value_after_restore])
 
     assert (
@@ -366,10 +370,16 @@ def test_restore_on_new_cluster(juju: Juju, charm, cloud_credentials, cloud_conf
     juju.deploy(
         charm,
         new_mysql_application_name,
-        num_units=1,
-        resources={"mysql-image": CHARM_METADATA["resources"]["mysql-image"]["upstream-source"]},
         base="ubuntu@22.04",
         config={"cluster-name": CLUSTER_NAME, "profile": "testing"},
+        num_units=1,
+        resources={"mysql-image": CHARM_METADATA["resources"]["mysql-image"]["upstream-source"]},
+        trust=True,
+    )
+
+    juju.wait(
+        ready=wait_for_apps_status(jubilant_backports.all_active, new_mysql_application_name),
+        timeout=TIMEOUT,
     )
 
     # relate to S3 integrator
@@ -385,33 +395,29 @@ def test_restore_on_new_cluster(juju: Juju, charm, cloud_credentials, cloud_conf
     # rotate all credentials
     logger.info("Rotating all mysql credentials")
 
-    primary_mysql = get_app_units(juju, new_mysql_application_name)[0]
-    primary_unit_address = get_unit_address(juju, new_mysql_application_name, primary_mysql)
+    primary_unit_name = get_mysql_primary_unit(juju, new_mysql_application_name)
+    primary_unit_address = get_unit_address(juju, new_mysql_application_name, primary_unit_name)
 
     rotate_mysql_server_credentials(
-        juju, primary_mysql, username=CLUSTER_ADMIN_USERNAME, password=CLUSTER_ADMIN_PASSWORD
+        juju, primary_unit_name, CLUSTER_ADMIN_USERNAME, CLUSTER_ADMIN_PASSWORD
     )
     rotate_mysql_server_credentials(
-        juju, primary_mysql, username=SERVER_CONFIG_USERNAME, password=SERVER_CONFIG_PASSWORD
+        juju, primary_unit_name, SERVER_CONFIG_USERNAME, SERVER_CONFIG_PASSWORD
     )
-    rotate_mysql_server_credentials(
-        juju, primary_mysql, username=ROOT_USERNAME, password=ROOT_PASSWORD
-    )
+    rotate_mysql_server_credentials(juju, primary_unit_name, ROOT_USERNAME, ROOT_PASSWORD)
 
-    server_config_credentials = get_mysql_server_credentials(juju, primary_mysql)
+    server_config_credentials = get_mysql_server_credentials(juju, primary_unit_name)
 
     # set the s3 config and credentials
     logger.info("Syncing credentials")
 
     juju.config(S3_INTEGRATOR, cloud_configs)
-    s3_unit = get_app_units(juju, S3_INTEGRATOR)[0]
-    task = juju.run(
-        unit=s3_unit,
+    s3_unit_name = get_app_units(juju, S3_INTEGRATOR)[0]
+    juju.run(
+        unit=s3_unit_name,
         action="sync-s3-credentials",
         params=cloud_credentials,
-        wait=5 * MINUTE_SECS,
     )
-    task.raise_on_failure()
 
     juju.wait(
         ready=wait_for_apps_status(
@@ -421,26 +427,22 @@ def test_restore_on_new_cluster(juju: Juju, charm, cloud_credentials, cloud_conf
     )
 
     logger.info("Waiting for blocked application status with another cluster S3 repository")
-    juju.wait(
-        ready=lambda status: (
-            status.apps[new_mysql_application_name].app_status.message
-            == ANOTHER_S3_CLUSTER_REPOSITORY_ERROR_MESSAGE
-        ),
+    juju.wait(  # Might take a few minutes to get past this
+        ready=lambda status: status.apps[new_mysql_application_name].app_status.message
+        == ANOTHER_S3_CLUSTER_REPOSITORY_ERROR_MESSAGE,
         timeout=TIMEOUT,
     )
 
     # restore the backup
     logger.info(f"Restoring {backup_id=}")
-    results = juju.run(
-        primary_mysql, "restore", params={"backup-id": backup_id}, wait=10 * MINUTE_SECS
-    )
-    results.raise_on_failure()
+
+    juju.run(primary_unit_name, "restore", params={"backup-id": backup_id})
 
     # ensure the correct inserted values exist
     logger.info(
         "Ensuring that the pre-backup inserted value exists in database, while post-backup inserted value does not"
     )
-    select_values_sql = [f"SELECT id FROM `{TEST_DATABASE_NAME}`.`{TABLE_NAME}`"]
+    select_values_sql = [f"SELECT id FROM `{DATABASE_NAME}`.`{TABLE_NAME}`"]
 
     values = execute_queries_on_unit(
         primary_unit_address,
@@ -463,6 +465,7 @@ def test_restore_on_new_cluster(juju: Juju, charm, cloud_credentials, cloud_conf
     )
 
     logger.info("Ensuring that pre-backup and post-restore values exist in the database")
+
     values = execute_queries_on_unit(
         primary_unit_address,
         server_config_credentials["username"],
@@ -473,29 +476,34 @@ def test_restore_on_new_cluster(juju: Juju, charm, cloud_credentials, cloud_conf
     assert sorted(values) == sorted([value_before_backup, value_after_restore])
 
     logger.info("Scaling mysql application to 3 units")
-    scale_app_units(juju, new_mysql_application_name, 3)
+    juju.add_unit(new_mysql_application_name, num_units=2)
     juju.wait(
-        ready=wait_for_apps_status(jubilant_backports.all_active, new_mysql_application_name),
+        ready=lambda status: all((
+            jubilant_backports.all_agents_idle(status, new_mysql_application_name),
+            *(
+                wait_for_unit_status(new_mysql_application_name, unit_name, "active")(status)
+                for unit_name in status.get_units(new_mysql_application_name)
+            ),
+        )),
         timeout=TIMEOUT,
     )
 
     logger.info("Ensuring inserted values before backup and after restore exist on all units")
-    for unit in get_app_units(juju, new_mysql_application_name):
-        mysql_unit_address = get_unit_address(juju, DATABASE_APP_NAME, unit)
+    for unit_name in get_app_units(juju, new_mysql_application_name):
+        unit_address = get_unit_address(juju, new_mysql_application_name, unit_name)
+
         values = execute_queries_on_unit(
-            mysql_unit_address,
+            unit_address,
             server_config_credentials["username"],
             server_config_credentials["password"],
             select_values_sql,
         )
-        assert value_before_backup
+
         assert sorted(values) == sorted([value_before_backup, value_after_restore])
 
     logger.info("Waiting for blocked application status after restore")
     juju.wait(
-        ready=lambda status: (
-            status.apps[new_mysql_application_name].app_status.message
-            == MOVE_RESTORED_CLUSTER_TO_ANOTHER_S3_REPOSITORY_ERROR
-        ),
+        ready=lambda status: status.apps[new_mysql_application_name].app_status.message
+        == MOVE_RESTORED_CLUSTER_TO_ANOTHER_S3_REPOSITORY_ERROR,
         timeout=TIMEOUT,
     )
